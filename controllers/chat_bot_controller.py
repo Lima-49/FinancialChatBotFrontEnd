4476ec0191e808845e44e45429f80f5693a5bddd
Lima-49 <vitoraugusto.soares@hotmail.com>
2025-10-14 18:20:47 -0300

    Adicionadndo o insert de pdf no chat

--- controllers/chat_bot_controller.py
+++ controllers/chat_bot_controller.py
@@ -1,29 +1,43 @@
+import re
 import requests
 
-def route_request(user_input):
-    # Regra simples: se contém "adicionar", "inserir", "novo gasto", envie para /insert
-    if any(word in user_input.lower() for word in ["adicionar", "inserir", "novo gasto", "comprar", "gastei"]):
-        return "/insert"
-    else:
-        return "/research"
-    
-def get_chat_response(prompt, chat_history=None):
-    url = "http://localhost:8000/api/v1"  # URL do endpoint do chatbot
-    
-    if chat_history is None:
-        chat_history = []
-    
-    
-    payload = {"query": prompt,
-               "chat_history": chat_history}
-    
-    headers = {"Content-Type": "application/json",
-               "x-api-key": "secretKey"}
-    
-    try:
-        route = route_request(prompt)
-        response = requests.post(url+route, json=payload, headers=headers)
-        response.raise_for_status()
-        return response.json().get("summary", "Desculpe, não consegui processar sua solicitação.")
-    except requests.exceptions.RequestException as e:
-        return f"Erro ao conectar com o chatbot: {e}"
+class ChatBotController:
+    def __init__(self):
+        self.url = "http://localhost:8000/api/v1"
+
+    def route_request(self, user_input, file_upload):
+        # Regra simples: se contém "adicionar", "inserir", "novo gasto", envie para /insert
+        if any(word in user_input.lower() for word in ["adicionar", "inserir", "novo gasto", "comprar", "gastei"]) and file_upload is None:
+            return "/insert"
+        elif file_upload is not None:
+            return "/insertPdf"
+        else:
+            return "/research"
+
+    def get_chat_response(self, prompt, chat_history=None, file_upload=None):
+        
+        if chat_history is None:
+            chat_history = []
+        
+
+        if file_upload is None:
+            payload = {"query": prompt,
+                    "chat_history": chat_history}
+        else:
+            payload = {
+                "query": prompt,
+                "chat_history": chat_history,
+                "file_path": file_upload.name,
+                "file_content": file_upload.getvalue().decode("latin-1"),
+            }
+
+        headers = {"Content-Type": "application/json",
+                "x-api-key": "secretKey"}
+        
+        try:
+            route = self.route_request(prompt, file_upload)
+            response = requests.post(self.url+route, json=payload, headers=headers)
+            response.raise_for_status()
+            return response.json().get("summary", "Desculpe, não consegui processar sua solicitação.")
+        except requests.exceptions.RequestException as e:
+            return f"Erro ao conectar com o chatbot: {e}"
